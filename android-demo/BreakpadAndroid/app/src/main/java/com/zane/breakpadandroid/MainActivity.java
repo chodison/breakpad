3642fa2ac8c4380bbd790badac40644476c33a0c
zaneCC <402075316@qq.com>
2017-08-11 16:26:08 +0800

    add client crash files

--- android-demo/BreakpadAndroid/app/src/main/java/com/zane/breakpadandroid/MainActivity.java
+++ android-demo/BreakpadAndroid/app/src/main/java/com/zane/breakpadandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zane.breakpadandroid;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.chodison.mybreakpad.ExceptionHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExceptionHandler.init(Environment.getExternalStorageDirectory().getAbsolutePath());
+    }
+
+    public void doClick(View view){
+
+        ExceptionHandler.testNativeCrash();
+
     }
 }

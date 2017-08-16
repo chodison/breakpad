e5719a11fe414995b7197b5415e754a3898240d4
chodison <c_soft_dev@163.com>
2017-08-16 10:25:21 +0800

    add support x86.

--- android-demo/BreakpadAndroid/breakpad/jni/Application.mk
+++ android-demo/BreakpadAndroid/breakpad/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-14
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 NDK_TOOLCHAIN_VERSION=4.9
 APP_STL := gnustl_static
 APP_CPPFLAGS += -std=gnu++11 -fexceptions

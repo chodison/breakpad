e9a64ee4308d73c1e4b9513218ced2b65abc6890
chodison <c_soft_dev@163.com>
2017-08-25 13:59:33 +0800

    [mybreakpad] add NativeMybreakpad.java Notes.

--- android-demo/BreakpadAndroid/breakpad/src/main/java/com/chodison/mybreakpad/NativeMybreakpad.java
+++ android-demo/BreakpadAndroid/breakpad/src/main/java/com/chodison/mybreakpad/NativeMybreakpad.java
@@ -5,12 +5,20 @@ import android.util.Log;
 
 /**
  * Created by chodison on 2017/8/22.
+ *
  */
 
 public class NativeMybreakpad {
     private static String TAG = "NativeMybreakpad";
 
     static boolean loadBreakpadSuccess = false;
+
+    /**
+     * 初始化返回类型
+     */
+    public enum BreakpadInitType {
+        TYPE_INIT_SUCCESS, TYPE_INIT_FAILED, TYPE_INIT_LOADSO_FAIL, TYPE_INIT_DUMPDIR_NULL
+    }
 
     static {
         try {
@@ -28,15 +36,24 @@ public class NativeMybreakpad {
         }
     }
 
-    public static boolean init(String dumpFileDir){
+    /**
+     * mybreakpad初始化,当初始化失败时，根据返回值确定原因，
+     * 若为TYPE_INIT_LOADSO_FAIL则此mybreakpad模块不可用
+     * @param dumpFileDir 崩溃发生时dump文件存储的目录
+     * @return
+     */
+    public static BreakpadInitType init(String dumpFileDir){
         if (TextUtils.isEmpty(dumpFileDir)) {
             Log.e(TAG, "dumpFileDir can not be empty");
-            return false;
+            return BreakpadInitType.TYPE_INIT_DUMPDIR_NULL;
         }
         if (loadBreakpadSuccess) {
-            return nativeInit(dumpFileDir) > 0 ;
+            if(nativeInit(dumpFileDir) > 0)
+                return BreakpadInitType.TYPE_INIT_SUCCESS;
+            else
+                return BreakpadInitType.TYPE_INIT_FAILED;
         }
-        return false;
+        return BreakpadInitType.TYPE_INIT_LOADSO_FAIL;
     }
 
     private static native int nativeInit(String dumpFileDir);
@@ -61,6 +78,7 @@ public class NativeMybreakpad {
     private native static int nativeDumpProcess(String dump_file, String processed_path, String[] check_sos);
 
     /**
+     * 仅当 {@link #dumpFileProcess}处理成功调取才有效
      * 获取崩溃的SO名称
      * @return
      */
@@ -73,6 +91,7 @@ public class NativeMybreakpad {
     private static native String[] nativeGetCrashSoName();
 
     /**
+     * 仅当 {@link #dumpFileProcess}处理成功调取才有效
      * 获取解析后崩溃的so堆栈地址
      * @return
      */

ef6d3f5efb07c392366c2de1792678e15027b711
chodison <c_soft_dev@163.com>
2020-02-24 09:38:45 +0800

    [mybreakpad] update version to V1.0.15.

--- android-demo/BreakpadAndroid/mybreakpad/src/main/java/com/chodison/mybreakpad/progma/BreakpadConfig.java
+++ android-demo/BreakpadAndroid/mybreakpad/src/main/java/com/chodison/mybreakpad/progma/BreakpadConfig.java
@@ -17,7 +17,7 @@ package com.chodison.mybreakpad.progma;
 
 
 public class BreakpadConfig {
-	public static final String BREAKPAD_LIB_VERSION = "V1.0.14";
+	public static final String BREAKPAD_LIB_VERSION = "V1.0.15";
 	/**
 	 * Print no output.
 	 */
